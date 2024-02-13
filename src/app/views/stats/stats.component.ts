8ac24a950e08fc3e8ec2c48ad411c69e4daadc93
Ranushan <ranusha.n@hotmail.fr>
2024-02-13 14:45:06 +0100

    change stats view

--- src/app/views/stats/stats.component.ts
+++ src/app/views/stats/stats.component.ts
@@ -4,7 +4,7 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Cat } from '@models/cat.model';
 import { CatService } from '@services/cat.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,49 +13,47 @@ import { finalize } from 'rxjs';
 @Component({
   selector: 'app-stats',
   standalone: true,
-  imports: [HttpClientModule, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatProgressSpinnerModule],
+  imports: [HttpClientModule, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatProgressBarModule],
   providers: [CatService],
   template: `
     <mat-form-field>
       <mat-label>Filter</mat-label>
-      <input matInput (keyup)="applyFilter($event)" placeholder="Ex. MTgwODA3MA" #input>
+      <input matInput (keyup)="applyFilter($event)" placeholder="Ex. Number of votes" #input>
     </mat-form-field>
 
     <div class="mat-elevation-z8">
-      @if(!dataLoading) {
-        <table mat-table [dataSource]="dataSource" matSort>
-          <!-- ID Column -->
-          <ng-container matColumnDef="id">
-            <th mat-header-cell *matHeaderCellDef mat-sort-header> ID </th>
-            <td mat-cell *matCellDef="let row"> {{row.id}} </td>
-          </ng-container>
-        
-          <!-- Progress Url -->
-          <ng-container matColumnDef="url">
-            <th mat-header-cell *matHeaderCellDef> Image URL </th>
-            <td mat-cell *matCellDef="let row"> <img style="width: 10em; height: 10em;" [src]="row.url"/> </td>
-          </ng-container>
-        
-          <!-- Name Votes -->
-          <ng-container matColumnDef="votes">
-            <th mat-header-cell *matHeaderCellDef mat-sort-header> Votes </th>
-            <td mat-cell *matCellDef="let row"> {{row.votes}} </td>
-          </ng-container>
-        
-          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-      
-          <!-- Row shown when there is no matching data. -->
-          <tr class="mat-row" *matNoDataRow>
+      <table mat-table [dataSource]="dataSource" matSort>
+        <!-- ID Column -->
+        <ng-container matColumnDef="id">
+          <th mat-header-cell *matHeaderCellDef mat-sort-header> ID </th>
+          <td mat-cell *matCellDef="let row"> {{row.id}} </td>
+        </ng-container>
+
+        <!-- URL Column -->
+        <ng-container matColumnDef="url">
+          <th mat-header-cell *matHeaderCellDef mat-sort-header> URL </th>
+          <td mat-cell *matCellDef="let row"> <img style="width: 10em; height: 10em;" [src]="row.url"/> </td>
+        </ng-container>
+
+        <!-- Votes Column -->
+        <ng-container matColumnDef="votes">
+          <th mat-header-cell *matHeaderCellDef mat-sort-header> Votes </th>
+          <td mat-cell *matCellDef="let row"> {{row.votes}} </td>
+        </ng-container>
+
+        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+        <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+
+        <!-- Row shown when there is no matching data. -->
+        <tr class="mat-row" *matNoDataRow>
+          @if(dataLoading && dataSource.data.length === 0) {
+            <td class="mat-cell-load" class="mat-cell-load" colspan="4"><mat-progress-bar mode="indeterminate"></mat-progress-bar></td>
+          } @else {
             <td class="mat-cell" colspan="4">No data matching the filter "{{input.value}}"</td>
-          </tr>
-        </table>
-      } @else {
-        <div style="display: flex; justify-content: center; align-items: center; text-align: center; min-height: 100vh;">
-          <mat-spinner></mat-spinner>
-        </div>
-      }
-      <mat-paginator [pageSizeOptions]="[5, 10, 25, 100]" aria-label="Select page of cat"></mat-paginator>
+          }
+        </tr>
+      </table>
+      <mat-paginator [pageSizeOptions]="[5, 10, 25, 100]" aria-label="Select page of cats"></mat-paginator>    
     </div>
   `,
   styles: `
@@ -101,12 +99,21 @@ export class StatsComponent implements OnInit {
         .subscribe({
           next  : res => {
             this.dataSource = new MatTableDataSource(res); // Assign the data to the data source for the table to render
+            this.dataSource.filterPredicate = this.filterPredicate;
             this.dataSource.paginator = this.paginator;
             this.dataSource.sort = this.sort;
           },
           error : err => console.error("Error when you getting pics.", err)
         });
   }
+
+  /**
+   * Filter Predicate from filter input
+   * @param data List of cats
+   * @param filter Filter by column
+   * @returns Return true if is equal otherwise false
+   */
+  private filterPredicate = (data: Cat, filter: string): boolean => data.votes.toString() === filter;
 
   /**
    * Filter from table
